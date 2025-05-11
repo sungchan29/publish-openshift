abf7852658f0c6bd0c11dfc9dfdcc942f152bb5c
sungchan29 <155981424+sungchan29@users.noreply.github.com>
2025-05-12 05:26:29 +0900

    update

--- ocp4/agent-based-install/abi-00-config-03-post-configuration-files/abi-00-create-post-configuration-file-02-apply-cm-cluster-monitoring-config.sh
+++ ocp4/agent-based-install/abi-00-config-03-post-configuration-files/abi-00-create-post-configuration-file-02-apply-cm-cluster-monitoring-config.sh
@@ -13,8 +13,10 @@ if ! source "$config_file"; then
     echo "[$(date +"%Y-%m-%d %H:%M:%S")] [ERROR] Failed to source '$config_file'. Check file syntax or permissions. Exiting..."
     exit 1
 fi
+if [[ "$MONITORING_CONFIG" == "true" ]]; then
+    echo "[$(date +"%Y-%m-%d %H:%M:%S")] [INFO] Monitoring configuration is disabled. Skipping ConfigMap creation."
 
-cat << EOF | ./oc apply -f -
+    cat << EOF | ./oc apply -f -
 apiVersion: v1
 kind: ConfigMap
 metadata:
@@ -25,72 +27,39 @@ data:
     alertmanagerMain:
       nodeSelector:
          node-role.kubernetes.io/infra: ""
-      tolerations:
-      - effect: NoSchedule
-        key: node-role.kubernetes.io/infra
-        operator: Exists
-    enableUserWorkload: false                  
+    enableUserWorkload: true                  
     kubeStateMetrics:
       nodeSelector:
         node-role.kubernetes.io/infra: ""
-      tolerations:
-      - effect: NoSchedule
-        key: node-role.kubernetes.io/infra
-        operator: Exists
     metricsServer:
       nodeSelector:
         node-role.kubernetes.io/infra: ""
-      tolerations:
-      - effect: NoSchedule
-        key: node-role.kubernetes.io/infra
-        operator: Exists
     monitoringPlugin:
       nodeSelector:
         node-role.kubernetes.io/infra: ""
-      tolerations:
-      - effect: NoSchedule
-        key: node-role.kubernetes.io/infra
-        operator: Exists
     openshiftStateMetrics:
       nodeSelector:
         node-role.kubernetes.io/infra: ""
-      tolerations:
-      - effect: NoSchedule
-        key: node-role.kubernetes.io/infra
-        operator: Exists
     prometheusK8s:
       nodeSelector:
-        node-role.kubernetes.io/infra: ""
-      tolerations:
-      - effect: NoSchedule
-        key: node-role.kubernetes.io/infra
-        operator: Exists                     
+        node-role.kubernetes.io/infra: ""                   
     prometheusOperator:
       nodeSelector:
         node-role.kubernetes.io/infra: ""
-      tolerations:
-      - effect: NoSchedule
-        key: node-role.kubernetes.io/infra
-        operator: Exists
     telemeterClient:
       nodeSelector:
         node-role.kubernetes.io/infra: ""
-      tolerations:
-      - effect: NoSchedule
-        key: node-role.kubernetes.io/infra
-        operator: Exists
     thanosQuerier:
       nodeSelector:
         node-role.kubernetes.io/infra: ""
-      tolerations:
-      - effect: NoSchedule
-        key: node-role.kubernetes.io/infra
-        operator: Exists
 EOF
 
-### Check if the ConfigMap is applied successfully
-if oc get configmap cluster-monitoring-config -n openshift-monitoring &> /dev/null; then
-    echo "[$(date +"%Y-%m-%d %H:%M:%S")] [INFO] ConfigMap 'cluster-monitoring-config' is applied successfully."
+    ### Check if the ConfigMap is applied successfully
+    if oc get configmap cluster-monitoring-config -n openshift-monitoring &> /dev/null; then
+        echo "[$(date +"%Y-%m-%d %H:%M:%S")] [INFO] ConfigMap 'cluster-monitoring-config' is applied successfully."
+    else
+        echo "[$(date +"%Y-%m-%d %H:%M:%S")] [ERROR] ConfigMap 'cluster-monitoring-config' is not applied successfully. Exiting..."
+    fi
 else
-    echo "[$(date +"%Y-%m-%d %H:%M:%S")] [ERROR] ConfigMap 'cluster-monitoring-config' is not applied successfully. Exiting..."
+    echo "[$(date +"%Y-%m-%d %H:%M:%S")] [INFO] Monitoring configuration is enabled. Skipping ConfigMap creation."
 fi
